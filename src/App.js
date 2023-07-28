dbf941e19640473bd5cec7fbfb991e682fcc2727
Simant <developersimant@gmail.com>
2023-07-28 20:53:25 +0545

    footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from './components/Home'
 import Header from './components/Header';
+import Footer from './components/footer/footer';
 function App() {
   return (
     <>
     <Header/>
     <Home/>
+    <Footer/>
     </>
   );
 }
