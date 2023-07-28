5028340ae10b6148a531b9992585c26bb0fcf6b4
Nishant <nishantkhatiwada6@gmail.com>
2023-07-28 20:30:42 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from './components/Home'
+import Header from './components/Header';
+function App() {
+  return (
+    <>
+    <Header/>
+    <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import "./Header.css"
+import Button from 'react-bootstrap/Button';
+
+const Header = () => {
+  return (
+    <>
+     <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Veda-clone</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Features</Nav.Link>
+            <NavDropdown title="Products" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Contact" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#link">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Button variant="outline-light" className='btn-1'>Request Demo</Button>{' '}
+      </Container>
+    </Navbar>
+    
+    </>
+  )
+}
+
+export default Header
