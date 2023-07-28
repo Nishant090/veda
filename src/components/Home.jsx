af69c04c3b9977807ba2936bf5286e2d6d23076c
Nishant <nishantkhatiwada6@gmail.com>
2023-07-28 21:04:36 +0545

    removing files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './home.css'
+import laptop from '../img/laptop.png'
+
 const Home = () => {
   return (
     <>
-    <div className='bg-img'></div>
+    <img src={laptop} alt='laptop' height='500px' width='500px'/>
+    <div className='bg-img'>
+        
+    </div>
       
     </>
   )
