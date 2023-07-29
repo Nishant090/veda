ec5ba52a659f189d9c8bb70d052fbd42c51ed1c6
Nishant <nishantkhatiwada6@gmail.com>
2023-07-29 13:30:35 +0545

    animatio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './home.css'
-import laptop from '../img/laptop.png'
+import man from '../img/man.png'
+import scatter from '../img/scatter.png'
 
 const Home = () => {
   return (
     <>
-    <img src={laptop} alt='laptop' height='500px' width='500px'/>
+    <img src={man} alt="phone" srcset="" height={'450px'} />
     <div className='bg-img'>
+    <img src={scatter} alt="phone" srcset="" height={'450px'} />
+      
+    </div>
+    <div className='bg-img2'></div>
+    
+    <div className='right1'>
+      <h1>Digital Learining and Managemnent System</h1>
+      <p>We are the best all-in-one cloud-based school software and digital learning system for growing, big and ambitious names in education.</p>
     </div>
 
 
